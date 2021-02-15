b6d2bdf914bb2bd764b024a7e8632d4c2c9e3b60
unknown <veloxbrcaldas@gmail.com>
2021-02-15 16:26:45 -0300

    primeiro commit

---
+++ templates/site.tpl.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Bem vindo a web</h1>
+    <?php include $content ?>
+
+    <p><small>2021 - todos os direitos reservados para o Geraldo Jamil!!!</small></p>
+</body>
+</html>
