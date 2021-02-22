e5f3d1eceb400c5095b99344e747bb2d9ca43d5d
jamil <veloxbrcaldas@gmail.com>
2021-02-22 17:53:43 -0300

    add

---
+++ templates/admin/pages/create.tpl.php
@@ -0,0 +1,5 @@
+<h3>Administração de Páginas</h3>
+
+//criação de páginas
+
+<a href="/admin">Ir para Home</a>
---
+++ templates/admin/pages/delete.tpl.php
@@ -0,0 +1,5 @@
+<h3>Administração de Páginas</h3>
+
+//remoção
+
+<a href="/admin">Ir para Home</a>
